fbd59b508a7e8c86599e9cf824a45c54555485ca
Joel <joelbraortiz@gmail.com>
2022-04-02 12:30:06 +0200

    started working

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import React, { useState } from "react";
+import Header from './components/header/Header'
+import Welcome from './components/Welcome/Welcome'
+
+export function App() {
+  return (
+    <>
+        <Header />
+        <Welcome />
+    </>
+    
+    )
+}
+
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './header.css'
+
+const Header = () => {
+    return(
+        <div class = "container-nav">
+            <h1>MateTIC</h1>
+        </div>
+        
+    )
+}
+
+export default Header
