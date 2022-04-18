63fab3c25664bf56b20f4c3c37fd4dd96058cf82
Joel <joelbraortiz@gmail.com>
2022-04-18 22:36:53 +0200

    added user page

--- client/src/components/User/UserPage.jsx
+++ client/src/components/User/UserPage.jsx
@@ -1,13 +1,42 @@
 import React from 'react'
+import img from '../home/resources/profile-photo.png'
+import school from './resources/school.png'
+import './user.css'
 
 const UserPage = () => {
   return (
     <div className="home-content">
         <div className="home-back">
-            <div className="home-header">
-                <h2>Datos del usuario</h2>
+          <div className="home-header">
+              <h2>Datos del usuario</h2>
+          </div>
+          <div className="user-data">
+            <div className="user-photo">
+              <img src= { img } alt="img-p" />
             </div>
-            
+            <div className="user-mail-name">
+              <h3>NOMBRE</h3>
+              <h4>name@mail.com</h4>
+            </div>
+            <div className="user-school">
+              <h3>Colegio Lorem ipsum</h3>
+              <div className="school-photo">
+                <img src= { school } alt="img-s" />
+              </div>
+            </div>
+          </div>
+          <div className="main-content">
+            <div className="option">
+              <p>Cambiar correo:</p>
+              <input className = "form-input" type="email" id="mail" name="mail" />
+              <button className = "btn">Cambiar</button>
+            </div>
+            <div className="option">
+              <p>Cambiar contraseña:</p>
+              <input className = "form-input" type="email" id="mail" name="mail" />
+              <button className = "btn">Cambiar</button>
+            </div>
+          </div>
         </div>
     </div>
   )
